20716bbb7eb57ed693387120f3d0518fb3842f07
Hungns243 <dauphailahung@gmail.com>
2025-05-07 11:21:55 +0700

    Initial Commit

---
+++ Cinema2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema2
+//
+//  Created by BeeCon on 7/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
